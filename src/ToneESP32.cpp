796aad9f260ea87370a63fdc748aeadf0100862a
weedmanu <teraspi13@gmail.com>
2023-02-02 13:11:10 +0100

    nouveau fichier : LICENSE.md 	nouveau fichier : examples/Simple_Tone/Simple_Tone.ino 	nouveau fichier : keywords.txt 	nouveau fichier : library.properties 	nouveau fichier : src/Notes.h 	nouveau fichier : src/ToneESP32.cpp 	nouveau fichier : src/ToneESP32.h

---
+++ src/ToneESP32.cpp
@@ -0,0 +1,22 @@
+#include "ToneESP32.h"
+
+ToneESP32::ToneESP32(int pin, int channel) {	
+  this->pin = pin;
+  this->channel = channel;  
+  ledcAttachPin( pin, channel );
+}
+
+
+
+void ToneESP32::tone(int note, int duree ) {     
+	ledcSetup( channel, note, PWM_Res );
+	ledcWrite( channel, 127 );                            
+    delay(duree);
+    ledcWrite( channel, 0 );
+}
+
+
+void ToneESP32::noTone() {
+    ledcDetachPin(pin);
+    ledcWrite(channel, 0);
+}
